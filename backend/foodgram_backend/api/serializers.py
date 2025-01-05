502ac10fd59403152f752b076ea5d4bc1f9b994a
Albina Shaibakova <albinashaibakova@gmail.com>
2025-01-05 16:12:28 +0500

    added api serializers

---
+++ backend/foodgram_backend/api/serializers.py
@@ -0,0 +1,46 @@
+from rest_framework import serializers
+from recipes.models import Ingredient, Favourite, Recipe, Tag, ShoppingCart
+
+
+class RecipeGetSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Recipe
+        fields = '__all__'
+
+
+class RecipeAddUpdateSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Recipe
+        fields = '__all__'
+
+    def perform_create(self, serializer):
+        serializer.save()
+        return serializer
+
+
+class TagSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Tag
+        fields = '__all__'
+
+
+class IngredientSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Ingredient
+        fields = '__all__'
+
+
+class ShoppingCartSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ShoppingCart
+        fields = ('recipe', 'user')
+
+
+class FavouriteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Favourite
+        fields = '__all__'
