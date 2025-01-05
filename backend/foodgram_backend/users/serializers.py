ae9c77fa6a94dddd6b1068973ca039bc50089448
Albina Shaibakova <albinashaibakova@gmail.com>
2025-01-05 16:12:13 +0500

    added users app serializers

---
+++ backend/foodgram_backend/users/serializers.py
@@ -0,0 +1,22 @@
+from django.contrib.auth import get_user_model
+
+from rest_framework import serializers
+
+from .models import Follow
+
+User = get_user_model()
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ('email', 'id', 'username', 'first_name',
+                  'last_name')
+
+
+class FollowSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Follow
+        fields = '__all__'
