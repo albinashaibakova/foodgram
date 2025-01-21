ecc9eb950aef108ff6fea909f961d12a5bde5de6
Albina Shaibakova <albinashaibakova@gmail.com>
2025-01-21 18:38:14 +0500

    modified filters

--- backend/api/filtersets.py
+++ backend/api/filtersets.py
@@ -1,17 +1,16 @@
-import django_filters
+from django_filters.rest_framework import filters, FilterSet
 
 from recipes.models import Recipe, Tag
 
 
-class RecipeFilterSet(django_filters.FilterSet):
-    username = django_filters.CharFilter(method='filter_author')
-    tags = django_filters.MultipleChoiceFilter(field_name='tags__slug',
-                                               lookup_expr='slug',
-                                               queryset=Tag.objects.all())
-    is_favorited = django_filters.CharFilter(
+class RecipeFilterSet(FilterSet):
+    tags = filters.MultipleChoiceFilter(field_name='tags__slug',
+                                        to_field_name='slug',
+                                        queryset=Tag.objects.all())
+    is_favorited = filters.CharFilter(
         method='filter_is_favorited'
     )
-    is_in_shopping_cart = django_filters.CharFilter(
+    is_in_shopping_cart = filters.CharFilter(
         method='filter_is_in_shopping_cart'
     )
 
@@ -22,13 +21,12 @@ class RecipeFilterSet(django_filters.FilterSet):
                   'is_favorited',
                   'is_in_shopping_cart')
 
-    def filter(self, queryset, name, value):
-        if value and self.request.user.id:
-            return queryset.filter(**{name: self.request.user})
+    def filter_is_favorited(self, queryset, name, value):
+        if self.request.user.is_authenticated and value:
+            return queryset.filter(favorites__user=self.request.user)
         return queryset
 
-     def filter_is_favorited(self, queryset, name, value):
-        return self.filter(queryset, name, value)
-
     def filter_is_in_shopping_cart(self, queryset, name, value):
-        return self.filter(queryset, name, value)
+        if self.request.user.is_authenticated and value:
+            return queryset.filter(shopping_cart__user=self.request.user)
+        return queryset
