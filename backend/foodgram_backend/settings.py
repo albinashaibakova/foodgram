0b05f2393ba96af9153d85f5b1be9a91d0f3407a
Albina Shaibakova <albinashaibakova@gmail.com>
2025-01-22 08:49:24 +0500

    nginx media

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -159,7 +159,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = '/var/www/foodgram/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
