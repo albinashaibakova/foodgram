8ea7bec9f76042b8b3de858edd27d4747bb6a97a
AlbinaShaiba <shaibakova@ptiufa.ru>
2025-01-28 08:38:10 +0500

    1-st review: fixed permissions.py

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -3,15 +3,6 @@ from rest_framework import permissions
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
 
-    def has_permission(self, request, view):
-        return (request.method in permissions.SAFE_METHODS
-                or request.user.is_authenticated)
-
     def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        if request.user.is_authenticated:
-            if request.user == obj.author:
-                return True
-            return False
-        return False
+        return (request.method in permissions.SAFE_METHODS
+                or request.user == obj.author)
