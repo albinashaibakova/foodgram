24f13082ba39b027e029e9fd9fbb61360df1fc4c
AlbinaShaiba <shaibakova@ptiufa.ru>
2025-02-12 18:09:53 +0500

    2-nd review: get-link

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import get_object_or_404
+from django.utils.text import slugify
 from rest_framework.response import Response
 
 from recipes.models import Recipe
 
 
-def get_recipe_short_link(request, recipe_id):
-    long_url = request.build_absolute_uri().split('api')[0]
-    slug = get_object_or_404(Recipe, id=request.kwargs['pk']).slug
+def get_recipe_short_link(request, pk):
+
+
+    long_url = request.build_absolute_uri()
+
+    my_string = get_object_or_404(Recipe, id=pk).name.translate(
+        str.maketrans(
+            "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ",
+            "abvgdeejzijklmnoprstufhzcss_y_euaABVGDEEJZIJKLMNOPRSTUFHZCSS_Y_EUA"
+    ))
+    slug = slugify(my_string)
+    print(slug)
+    long_url = request.build_absolute_uri(slug)
+    print(long_url)
     return Response({'short-link': f'{long_url}{slug}'})
