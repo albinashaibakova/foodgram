3059eab2ca8561bc73025da0c7b5a7c5a3c1bc6b
Albina Shaibakova <albinashaibakova@gmail.com>
2025-02-25 18:27:38 +0500

    3rd review: to deploy

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -7,7 +7,6 @@ from rest_framework.exceptions import ValidationError
 def get_short_link(request, pk):
     scheme = 'https' if request.is_secure() else 'http'
     domain = request.META['HTTP_HOST']
-    full_url = f'{scheme}://{domain}/recipes/{pk}/'
     if Recipe.objects.filter(id=pk).exists():
-        return redirect(full_url)
+        return redirect(f'{scheme}://{domain}/recipes/{pk}/')
     raise ValidationError(f'Рецепт по ключу {pk} не найден')
