236df0139af0fcc5d1da408fadd8bafb881ed3d8
Albina Shaibakova <albinashaibakova@gmail.com>
2025-02-26 20:02:36 +0500

    4th review: pep8+to deploy

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from rest_framework import pagination
 
+
 class FoodgramPaginator(pagination.PageNumberPagination):
     page_size_query_param = 'limit'
     page_size = getattr(settings, 'PAGE_SIZE', 6)
